6819b869f6fd2812ecacc42016feeb5dd7f771fb
david bahtiar <52740811+davidbahtiar55@users.noreply.github.com>
2022-06-22 15:17:33 +0700

    Pertemuan 5
    Materi class name, css selector, new tab minimize & maxsimize

--- pert1.py
+++ pert1.py
@@ -1,12 +1,44 @@
 from selenium import webdriver
+#from selenium.webdriver.common.by import By
+import time
 
 driver = webdriver.Chrome()
 
-driver.get("https://the-internet.herokuapp.com/login")
+driver.get("https://the-internet.herokuapp.com")
 #driver.find_element_by_name("username").send_keys("qaengineer")
 # find_element = untuk mencari single elemen __ dilanjutkan dengan action
 #driver.find_element_by_partial_link_text("Selenium").click()
 
 #Elements
-link = driver.find_elements_by_tag_name("a")
-print(len(link))
+# link = driver.find_elements_by_tag_name("a")
+# print(len(link))
+
+# Class Name
+#driver.find_element_by_class_name("radius").click()
+
+#CSS Selector
+#driver.find_element_by_css_selector("button.radius").click()
+
+#Via Copy Selector
+#driver.find_element_by_css_selector("#content > div > button").click()
+#driver.find_element_by_xpath('//*[@id="content"]/div/button').click()
+#driver.find_element(By.XPATH,'//*[@id="content"]/div/button').click()
+
+#New Tab
+# time.sleep(2)
+# driver.find_element_by_link_text("Click Here").click()
+# time.sleep(2)
+# driver.switch_to_window(driver.window_handles[0])
+# driver.find_element_by_link_text("Click Here").click()
+# time.sleep(2)
+# driver.switch_to_window(driver.window_handles[0])
+# driver.find_element_by_link_text("Click Here").click()
+# time.sleep(2)
+# driver.switch_to_window(driver.window_handles[0])
+# driver.find_element_by_link_text("Click Here").click()
+# time.sleep(2)
+# driver.switch_to_window(driver.window_handles[0])
+
+#Minimize & Maximize Windows
+#driver.maximize_window()
+#driver.minimize_window()
